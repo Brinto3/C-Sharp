922c4a7188665fff8f1777b07a491da1b261fa83
Brinto3 <113965654+Brinto3@users.noreply.github.com>
2025-05-12 11:23:16 +0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace EEMS
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtUserId;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblUserId;
+        private System.Windows.Forms.Label lblPassword;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtUserId = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblUserId = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // lblUserId
+            this.lblUserId.AutoSize = true;
+            this.lblUserId.Location = new System.Drawing.Point(30, 10);
+            this.lblUserId.Name = "lblUserId";
+            this.lblUserId.Size = new System.Drawing.Size(60, 17);
+            this.lblUserId.Text = "User ID:";
+
+            // txtUserId
+            this.txtUserId.Location = new System.Drawing.Point(30, 30);
+            this.txtUserId.Name = "txtUserId";
+            this.txtUserId.Size = new System.Drawing.Size(200, 22);
+
+            // lblPassword
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(30, 55);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(73, 17);
+            this.lblPassword.Text = "Password:";
+
+            // txtPassword
+            this.txtPassword.Location = new System.Drawing.Point(30, 75);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(200, 22);
+            this.txtPassword.PasswordChar = '*';
+
+            // btnLogin
+            this.btnLogin.Location = new System.Drawing.Point(30, 115);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(200, 30);
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(284, 171);
+            this.Controls.Add(this.lblUserId);
+            this.Controls.Add(this.txtUserId);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "Form1";
+            this.Text = "Login - EEMS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
